72b1f37f89807fde0d93911d5bc034d40a925804
MagdiDeveloper <mgdi.mahagny@gmail.com>
2022-06-15 21:37:10 +0300

    Delete SpaceLocationClient/src directory

--- SpaceLocationClient/src/app/models/location.ts
+++
@@ -1,6 +0,0 @@
-export class Location {
-    public  Longitude : number | undefined;
-    public  Latitude : number  | undefined;
-        public  CurrentTime : Date | undefined  ;
-        public  Notes: string = "";
-}
--- SpaceLocationClient/src/models/response.ts
+++
@@ -1,9 +0,0 @@
-export class Response<T>{
-
-  public Data : Data<T> | undefined
-}
-export class Data<T> {
-    public  IsSuccess : boolean = false;
-    public  Result :T | undefined;
-    public  Message : string = ""
-}
