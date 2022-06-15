0bf04b11eaa7afc2318df45126c800777d9396d0
MagdiDeveloper <mgdi.mahagny@gmail.com>
2022-06-15 21:45:44 +0300

    Add files via upload

---
+++ SpaceLocationClient/src/app/models/location.ts
@@ -0,0 +1,6 @@
+export class Location {
+    public  Longitude : number | undefined;
+    public  Latitude : number  | undefined;
+        public  CurrentTime : Date | undefined  ;
+        public  Notes: string = "";
+}
